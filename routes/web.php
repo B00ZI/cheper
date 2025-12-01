1a75e238cb6d695a26e7390b34c00349be4e6c21
B00ZI <iraamaneyahya@gmail.com>
2025-12-01 13:55:05 +0100

    adding a chirper controller

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,9 @@
 <?php
 
+use App\Http\Controllers\ChirperController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [ChirperController::class, 'index']);
+
+
+
