be1bf828513aff9471a19c0eedbf56f6447f76a9
B00ZI <iraamaneyahya@gmail.com>
2026-01-03 14:27:51 +0100

    Add edit, update, and delete functionality for chirps; enhance edit view with form validation

--- routes/web.php
+++ routes/web.php
@@ -5,5 +5,8 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', [ChirperController::class, 'index']);
 Route::post('/chirps', [ChirperController::class, 'store']);
+Route::get('/chirps/{chirp}/edit', [ChirperController::class, 'edit']);
+Route::put('/chirps/{chirp}', [ChirperController::class, 'update']);
+Route::delete('/chirps/{chirp}', [ChirperController::class, 'destroy']);
 
 
