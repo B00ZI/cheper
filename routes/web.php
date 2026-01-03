bf6b4b1fcb6bc287f03f27f9c4b803f9bcb1d9b3
B00ZI <iraamaneyahya@gmail.com>
2026-01-03 02:06:39 +0100

    Implement chirp posting functionality with validation and success message display

--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,6 @@ use App\Http\Controllers\ChirperController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', [ChirperController::class, 'index']);
-
+Route::post('/chirps', [ChirperController::class, 'store']);
 
 
